6d6f5cfd643ed833fba81772abc3b4347f2043b3
IIGang <rive449@gmail.com>
2019-11-28 20:19:38 +0900

    modififing idcheck view system

--- id_check.js
+++ id_check.js
@@ -1,22 +1,36 @@
-﻿$(function(){
-    
+﻿$(function () {
+
     var id = $('.id_tbox');
-    var pwd =$('.pwd_tbox');
-    var name =$('.name_tbox');
-    var email =$('.email_tbox');
+    var pwd = $('.pwd_tbox');
+    var name = $('.name_tbox');
+    var email = $('.email_tbox');
     var idCheck = $('.idCheck');
     var placeholderval;
+    var formdata;
 
-    
-    
-    $(".memcheck_button").click(function(){
+
+    $(".memcheck_button").click(function () {
         placeholderval = id.attr('placeholder');
-        console.log(id.val());
+        formdata= $(".logform").serializeArray();
+        console.log(formdata);
         console.log(placeholderval);
-      
 
-	$.post( "id_check.php", {id:id.val()}, function(data){if(data){ $('.id_tbox').attr("placeholder", data);}});
+
+        $.post("id_check.php", 
+            formdata
             
-            
+        , function (data) {
+            if (data) {
+                console.log(data);
+                if(data.idCheck =='1'){
+                $('.id_tbox').attr("placeholder", data);
+                }
+                else{
+                $('.id_tbox').attr("placeholder", "sssss");
+                }
+            }  
         });
+
+
     });
+});
