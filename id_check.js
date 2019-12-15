32e6ca9cef3a567662a2adf59e6069609d0b3171
IIGang <river449@gmail.com>
2019-12-15 16:28:14 +0900

    adding idcheck border view

--- id_check.js
+++ id_check.js
@@ -8,12 +8,15 @@
     var placeholderval;
     var formdata;
     var trigger = true;
+    var count = 0;
     //var jsn;
     //var jsn2
 
     $(".memcheck_button").click(function () {
         placeholderval = id.attr('placeholder');
         formdata = $(".logform").serializeArray();
+        count = 0;//여러번 누리면 계속 누적되서 적용되는거 수정해야힘
+    
 
 
         $.post("id_check.php",
@@ -22,51 +25,37 @@
             ,
             function (data) {
                 if (data) {
-                    
-                     
-                    
-                    setInterval(toggle,500);  
-                    setTimeout(st,1000);
-                    //clearInterval(toggle);
-                    
-                    function toggle() {
 
-                        if (trigger) {
-
-                            $(".id_check_border").fadeOut(500);
-                            trigger = false;
-                        } else {
-
-                            $(".id_check_border").fadeIn(500);
-                            trigger = true;
-                        }
-
-                    }
-                    
-                    var st = function stop_ani()
-                    {
-
-                        clearInterval(toggle);
-                    }
-                   
                     console.log(data);
                     console.log(data.idCheck);
                     // jsn = JSON.stringify(data);
                     //console.log(jsn[17]); json으로 안받고 그냥 text로 받앗을때 무식하게 문자열 잘라서 쓴거
                     if (data.idCheck === "1") {
+                        $(".id_check_border").fadeIn(500);
+
                         $(".id_check_p1").show();
                         $(".sec1").addClass("on");
+
+                        setInterval(toggle, 500);
+                        //clearInterval(toggle);
+
                         $(".id_check_p1").text("이미 존해하는 아이디 입니다.");
                         $(".id_tbox").val("");
+                        
                     } else if (data.idCheck === "0") {
                         $(".id_check_p1").show();
                         $(".sec1").addClass("on");
                         $(".id_check_p1").text("사용 가능한 아이디입니다.");
                         // $('.id_tbox').attr("placeholder", "사용가능한 아이디입니다.");
                     } else {
+
                         $(".sec1").removeClass("on");
+                        $(".id_check_border").fadeIn(500);
                         $(".id_check_p1").hide();
+                        setInterval(toggle, 500);
+                        //clearInterval(toggle);
                         $('.id_tbox').attr("placeholder", "아이디를 적어 주십시오");
+                       
                     }
 
 
@@ -75,4 +64,27 @@
 
 
     });
+
+    function toggle() {
+        
+        if (count == 2) {
+
+            clearInterval(toggle);
+        } else {
+
+
+            if (trigger) {
+
+                $(".id_check_border").fadeOut(500);
+                trigger = false;
+                count++;
+            } else {
+
+                $(".id_check_border").fadeIn(500);
+                trigger = true;
+                
+            }
+
+        }
+    }
 });
